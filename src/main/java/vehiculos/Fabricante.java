ce71a6d5528d6f0d98ffc39faa208f4b302c4a2d
pablo <juaescobarm@gmail.com>
2021-04-21 03:29:11 +0200

    m

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,47 +1,49 @@
 package vehiculos;
+
 import java.util.ArrayList;
 
 public class Fabricante {
+    private String nombre;
+    private Pais pais;
+    static ArrayList<Fabricante> fabricantes = new ArrayList<>();
+    public int venta;
 
-    String nombre;
-    Pais pais;
-    int vendidos;
-    static  ArrayList<Fabricante> ArrayFabricantes= new ArrayList<>();
-    public  Fabricante(String nombre,Pais pais){
-        this.nombre=nombre;
-        this.pais=pais;
-        ArrayFabricantes.add(this);
-
+    public Fabricante(String nombre, Pais pais){
+        this.nombre = nombre;
+        this.pais = pais;
+        fabricantes.add(this);
     }
     public Fabricante(){
 
     }
+
     public void setNombre(String nombre){
-        this.nombre=nombre;
-    }
-    public String getNombre(){
-        return  nombre;
+        this.nombre = nombre;
     }
     public void setPais(Pais pais){
-        this.pais=pais;
-    }
-    public Pais getPais(){
-        return  pais;
-    }
-    public static Fabricante fabricaMayorVentas(){
-        int mayor = 0;
-        for(Fabricante fabricante : ArrayFabricantes){
-            if (fabricante.vendidos > mayor){
-                mayor = fabricante.vendidos;
-            }
-        }
-        Fabricante FabricanteMasVendido = new Fabricante();
-        for(Fabricante fabricante : ArrayFabricantes){
-            if(fabricante.vendidos == mayor){
-                FabricanteMasVendido = fabricante;
-            }
-        }
-        return FabricanteMasVendido;
+        this.pais = pais;
     }
 
+    public String getNombre(){
+        return nombre;
+    }
+    public Pais getPais(){
+        return pais;
+    }
+
+    public static Fabricante fabricaMayorVentas(){
+        int mayor = 0;
+        for(Fabricante fabricante : fabricantes){
+            if (fabricante.venta > mayor){
+                mayor = fabricante.venta;
+            }
+        }
+        Fabricante MayorF = new Fabricante();
+        for(Fabricante fabricanteM : fabricantes){
+            if(fabricanteM.venta == mayor){
+                MayorF = fabricanteM;
+            }
+        }
+        return MayorF;
+    }
 }
